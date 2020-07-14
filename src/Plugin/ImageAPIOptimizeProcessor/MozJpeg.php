faad25643b8366ad70fc77ba341d92006997360c
Richard Sartori <richard.sartori@gmail.com>
2020-07-14 14:33:37 +0200

    mozjpeg

---
+++ src/Plugin/ImageAPIOptimizeProcessor/MozJpeg.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\imageapi_optimize_binaries\Plugin\ImageAPIOptimizeProcessor;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\imageapi_optimize_binaries\ImageAPIOptimizeProcessorBinaryBase;
+
+/**
+ * Uses the MozJpeg binary to optimize images.
+ *
+ * @ImageAPIOptimizeProcessor(
+ *   id = "mozjpeg",
+ *   label = @Translation("MozJpeg"),
+ *   description = @Translation("Uses the MozJpeg binary to optimize images.")
+ * )
+ */
+class MozJpeg extends ImageAPIOptimizeProcessorBinaryBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function executableName() {
+    return 'mozjpeg';
+  }
+
+  public function applyToImage($image_uri) {
+    if ($cmd = $this->getFullPathToBinary()) {
+      $dst = $this->sanitizeFilename($image_uri);
+
+      if ($this->getMimeType($image_uri) == 'image/jpeg') {
+        $options = array(
+          '-optimize',
+        );
+
+        if (is_numeric($this->configuration['quality'])) {
+          $options[] = '-quality ' . escapeshellarg($this->configuration['quality']);
+        }
+
+        if ($this->configuration['progressive']) {
+          $options[] = '-progressive';
+        }
+
+        $arguments = array(
+          $dst,
+        );
+
+        $option_string = implode(' ', $options);
+        $argument_string = implode(' ', array_map('escapeshellarg', $arguments));
+        return $this->saveCommandStdoutToFile(escapeshellarg($cmd) . ' ' . $option_string . ' ' . $argument_string, $dst);
+      }
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+      'progressive' => TRUE,
+      'quality' => 90,
+    ];
+  }
+
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $form['progressive'] = array(
+      '#title' => $this->t('Progressive'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['progressive'],
+    );
+
+    $form['quality'] = array(
+      '#title' => $this->t('Quality'),
+      '#type' => 'number',
+      '#min' => 0,
+      '#max' => 100,
+      '#description' => $this->t('Compression quality (0..100; 5-95 is most useful range, default is 75)'),
+      '#default_value' => $this->configuration['quality'],
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+
+    $this->configuration['progressive'] = $form_state->getValue('progressive');
+    $this->configuration['quality'] = $form_state->getValue('quality');
+  }
+}
